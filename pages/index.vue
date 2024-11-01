21e512335cd097d450d085cdf6983ca741259b67
Emiliano <emiliano.0218@gmail.com>
2024-10-31 23:13:29 -0600

    add styles

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,10 @@ const ttldays = ref(null);
 
 const handlePlusDays = (ttlday) => {
   ttldays.value = ttlday;
-  console.log(dates.value);
+  console.log(ttldays.value);
+  if (!dates.value || dates.value[0] === null || dates.value[1] === null) {
+    return;
+  }
   const start = dates.value[0];
   const end = dates.value[1];
   switch (ttlday) {
@@ -33,37 +36,73 @@ const handlePlusDays = (ttlday) => {
   }
 };
 
+const shouldDisableDate = () => {
+  if (!dates.value || dates.value[0] === null || dates.value[1] === null) {
+    return true;
+  }
+  return false;
+};
+
+const parseFormatDate = (date) => {
+  if (!date) {
+    return "";
+  }
+  return new Date(date).toLocaleDateString("es-MX", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+}; 
 </script>
 <template>
   <main>  
     <h4 class="text-green-700 font-bold text-sm mx-auto text-center">Calendar </h4>
     <div class="bg-gray-100 rounded-sm shadow-sm max-w-md min-h-[600px] mx-auto flex justify-center p-5 ">
-      <div>
-        <div class="flex mb-2">
+      <div class="flex flex-col gap-y-3">
+        <div class="flex mb-2 justify-center">
           <button 
             @click="handlePlusDays(0)"
-            :class="ttldays === 0 ? 'bg-green-700 text-white' : 'bg-white text-black' "
-            class="bg-white flex justify-center items-center border border-gray-200 px-2 py-1 min-w-9 rounded-l">0</button>
+            :disabled="shouldDisableDate()"
+            :class="[
+              ttldays == 0 ? 'bg-green-700 text-white' : 'bg-white text-black',
+              shouldDisableDate() ? 'cursor-not-allowed text-gray-300' : ''
+            ]"
+            class=" flex justify-center items-center border border-gray-200 px-2 py-1 min-w-9 rounded-l">0</button>
           <button 
             @click="handlePlusDays(1)"
-            :class="ttldays === 1 ? 'bg-green-700 text-white' : 'bg-white text-black' "
-            class="bg-white flex justify-center items-center border border-gray-200 px-2 py-1 min-w-9">1</button>
+            :class="[
+              ttldays == 1 ? 'bg-green-700 text-white' : 'bg-white text-black',
+              shouldDisableDate() ? 'cursor-not-allowed text-gray-300' : ''
+            ]"
+            class=" flex justify-center items-center border border-gray-200 px-2 py-1 min-w-9">1</button>
           <button 
             @click="handlePlusDays(3)"
-            :class="ttldays === 3 ? 'bg-green-700 text-white' : 'bg-white text-black' "
-            class="bg-white flex justify-center items-center border border-gray-200 px-2 py-1 min-w-9">3</button>
+            :class="[
+              ttldays == 3 ? 'bg-green-700 text-white' : 'bg-white text-black',
+              shouldDisableDate() ? 'cursor-not-allowed text-gray-300' : ''
+            ]"
+            class=" flex justify-center items-center border border-gray-200 px-2 py-1 min-w-9">3</button>
           <button 
             @click="handlePlusDays(5)"
-            :class="ttldays === 5 ? 'bg-green-700 text-white' : 'bg-white text-black' "
-            class="bg-white flex justify-center items-center border border-gray-200 px-2 py-1 min-w-9">5</button>
+            :class="[
+              ttldays == 5 ? 'bg-green-700 text-white' : 'bg-white text-black',
+              shouldDisableDate() ? 'cursor-not-allowed text-gray-300' : ''
+            ]"
+            class=" flex justify-center items-center border border-gray-200 px-2 py-1 min-w-9">5</button>
           <button 
             @click="handlePlusDays(7)"
-            :class="ttldays === 7 ? 'bg-green-700 text-white' : 'bg-white text-black' "
-            class="bg-white flex justify-center items-center border border-gray-200 px-2 py-1 min-w-9">7</button>
+            :class="[
+              ttldays == 7 ? 'bg-green-700 text-white' : 'bg-white text-black',
+              shouldDisableDate() ? 'cursor-not-allowed text-gray-300' : ''
+            ]"
+            class=" flex justify-center items-center border border-gray-200 px-2 py-1 min-w-9">7</button>
           <button 
             @click="handlePlusDays(15)"
-            :class="ttldays === 15 ? 'bg-green-700 text-white' : 'bg-white text-black' "
-            class="bg-white flex justify-center items-center border border-gray-200 px-2 py-1 min-w-9 rounded-r">15</button>
+            :class="[
+              ttldays == 15 ? 'bg-green-700 text-white' : 'bg-white text-black',
+              shouldDisableDate() ? 'cursor-not-allowed text-gray-300' : ''
+            ]"
+            class=" flex justify-center items-center border border-gray-200 px-2 py-1 min-w-9 rounded-r">15</button>
         </div>
         <DatePicker 
           v-model="dates" 
@@ -72,6 +111,10 @@ const handlePlusDays = (ttlday) => {
           showWeek 
           :manualInput="false" 
           class="block"/>
+
+        <span class="bg-gray-300 h-8 w-full rounded-md flex justify-center items-center text-green-950 text-sm" v-if="dates">
+          {{ parseFormatDate(dates[0]) }} - {{ parseFormatDate(dates[1]) }}
+        </span>
       </div>
     </div>
   </main>
